64d7f5c9ad21d7eacee7eba51404d4a24914551a
mzp <mzpppp@gmail.com>
2010-11-01 22:34:14 +0900

    オブジェクトの定義を追加 refs 2

--- proof/Object.v
+++ proof/Object.v
@@ -1,8 +1,18 @@
 Require Import List.
+Require Import Pow.
 
+(* ** MsgPackで使うオブジェクトの定義 *)
 Inductive object :=
-| Bool    (_ : bool)
-| Array16 ( _ : list object).
+| Bool (_ : bool)
+| Array16  ( _ : list object).
 
-
+(** * 妥当なオブジェクトの定義 *)
+Inductive Valid : object -> Prop :=
+| VArray16Nil  :
+  Valid (Array16 nil)
+| VArray16Cons : forall x xs,
+  Valid x ->
+  Valid (Array16 xs) ->
+  List.length (x::xs) < pow 16 ->
+  Valid (Array16 (x::xs)).
 
