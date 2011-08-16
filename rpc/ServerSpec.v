01085a96fd37aed5b3b2a69d6fcbc9f7e6afbdcf
mzp <mzpppp@gmail.com>
2011-08-16 09:55:32 +0900

    Serverの定義を変更

--- rpc/ServerSpec.v
+++ rpc/ServerSpec.v
@@ -1,16 +1,17 @@
 Add LoadPath "../applpi".
 Add LoadPath "../proof".
 
-Require Import List String.
 Require Import libapplpi.
-Require Import Object Session.
+Require Import Session.
+
+Inductive ServerSpec' : session -> proc -> Prop :=
+| SCall   : forall req res val,
+  ServerSpec' (Call req res) (OutAtom res val)
+| SNotify : forall n,
+  ServerSpec' (Notify n) zeroP.
 
 Inductive ServerSpec : proc -> Prop :=
-| SCall   : forall f b (ch : chan session b),
-  (forall req res, ServerSpecAfterCall res (f (Call req res))) ->
-  ServerSpec (ch ?? f)
-| SNotify : forall f b (ch : chan session b),
-  (forall n, ServerSpec (f (Notify n))) -> ServerSpec (ch ?? f)
-with ServerSpecAfterCall : chan response true -> proc -> Prop :=
-  ServerSPecAfterCall : forall P (ch : chan response true) rval,
-    ServerSpec P -> ServerSpecAfterCall ch (ch << rval >> P).
+| SServer : forall b (ch : chan session b) P,
+  (forall msg, ServerSpec' msg (P msg)) ->
+  ServerSpec (ch ??* (fun msg => P msg)).
+
