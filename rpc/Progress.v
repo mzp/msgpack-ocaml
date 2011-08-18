98aa51beca11f2f94bbb4372411dbf597f87f427
mzp <mzpppp@gmail.com>
2011-08-18 09:50:23 +0900

    [from now] 2011年 8月18日 木曜日 09時50分23秒 JST
    diff --git a/rpc/Progress.v b/rpc/Progress.v
    index f3a3269..60d5342 100644
    --- a/rpc/Progress.v
    +++ b/rpc/Progress.v
    @@ -14,3 +14,55 @@ Theorem rpc_progress : forall res S C f,
       (forall ch, ClientSpec ch res (C ch)) ->
       Progress res f ->
       (nilC # nuPl (fun c => parP (S c) (C c))) |=t (FMUSTEV (ALWAYS (STAT f))).
    +Proof with auto.
    +intros res S C f SS CS FS.
    +apply cong_resp_tsat with (parP zeroP
    +  (nuPl (fun c : chan session true => parP (S c) (C c)))).
    + (* subgoal 1*)
    + apply red_new_deterl.
    + CheckStable; [ simpl | simpl | CheckInc]...
    + intros.
    + specialize (SS c).
    + specialize (CS c).
    + destruct SS; destruct CS.
    +  (* call case *)
    +  destruct H1.
    +  apply red_com_deter_rinout.
    +   intro.
    +   apply INPUTS_sat_inv  in H2.
    +   decompose [ex and] H2.
    +   apply cong_zero_zeros_only in H3.
    +   assert (zeros_only zeroP); [apply one_zero|].
    +   apply H3 in H4.
    +   inversion H4.
    +   inversion H8.
    +
    +   intro.
    +   decompose[ex] H2.
    +   apply OUTPUTS_sat_inv in H3.
    +   decompose [ex and] H3.
    +   apply cong_zero_zeros_only in H4.
    +   assert (zeros_only zeroP); [apply one_zero|].
    +   apply H4 in H5.
    +   inversion H5.
    +   inversion H9.
    +
    +   CheckStable; [ simpl | simpl | CheckInc]...
    +
    +
    +   CheckStable.
    +
    +  CheckExistsGuard.
    +CheckUnstable.
    +intros.
    +NextComms.
    +simpl in |- *.
    +intros.
    +or_false.
    +SolveINPUTS.
    +
    +  (* notify case *)
    +
    + case H.
    +
    + (* subgoal 2: Cong ... *)

--- rpc/Progress.v
+++ rpc/Progress.v
@@ -14,3 +14,55 @@ Theorem rpc_progress : forall res S C f,
   (forall ch, ClientSpec ch res (C ch)) ->
   Progress res f ->
   (nilC # nuPl (fun c => parP (S c) (C c))) |=t (FMUSTEV (ALWAYS (STAT f))).
+Proof with auto.
+intros res S C f SS CS FS.
+apply cong_resp_tsat with (parP zeroP
+  (nuPl (fun c : chan session true => parP (S c) (C c)))).
+ (* subgoal 1*)
+ apply red_new_deterl.
+ CheckStable; [ simpl | simpl | CheckInc]...
+ intros.
+ specialize (SS c).
+ specialize (CS c).
+ destruct SS; destruct CS.
+  (* call case *)
+  destruct H1.
+  apply red_com_deter_rinout.
+   intro.
+   apply INPUTS_sat_inv  in H2.
+   decompose [ex and] H2.
+   apply cong_zero_zeros_only in H3.
+   assert (zeros_only zeroP); [apply one_zero|].
+   apply H3 in H4.
+   inversion H4.
+   inversion H8.
+
+   intro.
+   decompose[ex] H2.
+   apply OUTPUTS_sat_inv in H3.
+   decompose [ex and] H3.
+   apply cong_zero_zeros_only in H4.
+   assert (zeros_only zeroP); [apply one_zero|].
+   apply H4 in H5.
+   inversion H5.
+   inversion H9.
+
+   CheckStable; [ simpl | simpl | CheckInc]...
+
+
+   CheckStable.
+
+  CheckExistsGuard.
+CheckUnstable.
+intros.
+NextComms.
+simpl in |- *.
+intros.
+or_false.
+SolveINPUTS.
+
+  (* notify case *)
+
+ case H.
+
+ (* subgoal 2: Cong ... *)
